81fe04a52bd013807f8b6d403b53c32575acdf94
RITESH SAHOO <124902036+rlly-ritesh@users.noreply.github.com>
2025-09-18 15:37:57 +0530

    first commit

---
+++ Basics of Python/hello.py
@@ -0,0 +1,12 @@
+def hello():
+    print("Hello, World!")
+    name = input("What is your name? ")
+    regn_no = input("What is your registration number? ")
+    roll_no = input("What is your roll number? ")
+    print("Regd No:", regn_no)
+    print("Roll No:", roll_no)
+    print("Welcome", name)
+hello()
+
+
+
