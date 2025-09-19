58f6159c3a27a404d0c5fa67ba383c81e1df34c8
RITESH SAHOO <124902036+rlly-ritesh@users.noreply.github.com>
2025-09-19 19:22:17 +0530

    More sets functionalities

--- Basics of Python/sets.py
+++ Basics of Python/sets.py
@@ -3,9 +3,22 @@ B = {1, 2, 3, 4, 5}
  
 # Set union using .union()
 print(A.union(B))
- 
 # Set union using the | operator
 print(A | B)
- 
 # Output:
 # {1, 2, 3, 4, 5, 6, 8}
+print(A.intersection(B))
+# Use the & operator
+print(A & B) 
+# Output:
+# {1, 2, 4}
+print(A.difference(B))
+# Use the - operator
+print(A - B)
+# Output:
+# {8, 6}
+print(B.difference(A))
+# Use the - operator
+print(B - A)
+# Output:
+# {3, 5}
